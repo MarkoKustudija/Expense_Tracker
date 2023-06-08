5a58422beb44c2dcb97eef921bee05721c2efe2d
marko <marko@marko.com>
2023-06-08 19:39:58 +0200

    small changes

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,7 +8,7 @@ import ExpensesChart from "./ExpensesChart";
 
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2023");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
